076efec90431c21a67706267901dfe552ec006e2
Hans Preeker <hans_preeker@hotmail.com>
2020-08-27 11:22:49 +0200

    FIXED DA MAP DUDE

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,7 +17,7 @@ class BooksController < ApplicationController
       {
         lat: book.latitude,
         lng: book.longitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { book: book }),
+        infoWindow: render_to_string(partial: "info_window", locals: { book: book }),
       }
     end
   end
